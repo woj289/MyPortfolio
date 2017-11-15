a4f6806643eece643a633e7ffb3fdc8a69583553
woj289 <woj289@gmail.com>
2017-11-15 13:44:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 
 function scroll_to(selector) {
     $('html,body').animate({scrollTop: $(selector).offset().top}, 1000);
     return false;
 }
-=======
 
-function scroll_to(selector) {
-    $('html,body').animate({scrollTop: $(selector).offset().top}, 1000);
-    return false;
-}
->>>>>>> 63a75959b0c01165cf5b8c8a8ea4a529dfae1704
