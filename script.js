c318a692093f46d29cdc19aa09ae24eaff93b984
woj289 <woj289@gmail.com>
2017-11-16 09:58:17 +0100

    1.0

--- script.js
+++
@@ -1,5 +0,0 @@
-
-function scroll_to(selector) {
-    $('html,body').animate({scrollTop: $(selector).offset().top}, 1000);
-    return false;
-}
--- send-script.php
+++
@@ -1,4 +0,0 @@
-<?php
-mail( $mailToSend, 'Wiadomość ze strony - ' . date( "d-m-Y" ), $message, $headers ) ) {
-	$return['status'] = 'ok';
-}
